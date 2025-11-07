7af42528cea57d4ce410c6878eecf3befd01966f
Aaronrthomas <aaronrthomas88@gmail.com>
2025-11-07 20:26:53 +0530

    changes made to footer section

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,18 +6,18 @@ export default function Footer() {
     return (
         <div className="bg-[#8265AB] text-white p-4 py-8 md:p-8 flex flex-col md:flex-row items-start justify-between gap-6" id="contact">
             <div className="flex flex-col gap-3 items-start">
-                <b className="font-libre">Copyright © Compassion AI • All Rights Reserved</b>
+                <b className="font-libre">Copyright © AI+ Compassion  • All Rights Reserved</b>
                 <b className="font-libre py-3">Have a specific question?</b>
                 <p>
                     General inquiries{" "}
-                    <Link className="underline" href="mailto:contact@compassionai.io">
-                        contact@compassionai.io
+                    <Link className="underline" href="mailto:connect@compassionai.io">
+                        connect@compassionai.io
                     </Link>
                 </p>
                 <p>
                     Academic partnerships{" "}
-                    <Link className="underline" href="mailto:contact@compassionai.io">
-                        contact@compassionai.io
+                    <Link className="underline" href="mailto:connect@compassionai.io">
+                        connect@compassionai.io
                     </Link>
                 </p>
                 <p>
