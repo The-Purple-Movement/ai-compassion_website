fba4423bd2ecef3fda9b17acc2d98b534dc5d507
Aaronrthomas <aaronrthomas88@gmail.com>
2025-11-24 20:40:29 +0530

    added google form link

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -192,7 +192,7 @@ export default function Navbar() {
           </div>
 
           <Link
-            href="https://docs.google.com/forms/d/e/1FAIpQLSe1DGS12mNg1nLPuawyoWMCoS4xmKRDKAv8_gAytJTpIJMx-g/viewform?usp=header"
+            href="https://forms.gle/7FJH6pNbKxgi11wt5"
             target="_blank"
             rel="noopener noreferrer"
             className=" padding-8px px-6 py-2 rounded-full bg-[#D55485] hover:bg-[#D55485]/80 transition-colors duration-200 text-white font-medium text-sm"
